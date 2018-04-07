7fe077b9c47e3d5e76324dd284fc979aa4b5570a
hsperr <henning.sperr@gmail.com>
2018-04-07 17:12:22 +0900

    Adds missing tests

--- game_clean.py
+++ game_clean.py
@@ -56,7 +56,36 @@ def test_empty_board_moves():
     moves  = board.moves()
     assert len(moves) == 18
 
+def test_board_sizes():
+    board = Board()
+    assert board.size_x == 6
+    assert board.size_y == 6
+
+def test_board_valid_position():
+    board = Board()
+    assert board.valid_position(0, 0)
+    assert board.valid_position(2, 3)
+    assert not board.valid_position(0, board.size_y)
+    assert not board.valid_position(board.size_x, 0)
+    assert not board.valid_position(board.size_x, board.size_y)
+    assert board.valid_position(board.size_x-1, 0)
+    assert board.valid_position(0, board.size_y-1)
+    assert board.valid_position(board.size_x-1, board.size_y-1)
+
+def test_board_neighbor_fields():
+    board = Board()
+    assert board.get_neighbor_fields(0, 0) == [(1,0), (0,1)]
+    assert board.get_neighbor_fields(1, 0) == [(2,0), (0,0), (1, 1)]
+    assert board.get_neighbor_fields(2, 3) == [(3, 3), (1, 3), (2, 4), (2, 2)]
+
+    board = Board(allow_diagonals=True)
+    assert board.get_neighbor_fields(0, 0) == [(1,0), (0,1), (1,1)]
+    assert board.get_neighbor_fields(2, 3) == [(3, 3), (1, 3), (2, 4), (2, 2), (3, 4), (3, 2), (1, 4), (1, 2)]
+
 
 test_empty_board_moves()
+test_board_sizes()
+test_board_valid_position()
+test_board_neighbor_fields()
             
                     
