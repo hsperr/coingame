1abaae19a25b5a91ab875dde95b726bffb798d2a
hsperr <henning.sperr@gmail.com>
2018-04-07 22:02:48 +0900

    Implement move function and partial tests

--- game_clean.py
+++ game_clean.py
@@ -29,31 +29,77 @@ class Board():
     def size_y(self):
         return len(self.board)
 
-    def get_neighbor_fields(self, x, y):
-        normal_moves =  [p for p in [(x+1, y), (x-1, y), (x, y+1), (x, y-1) ] if self.valid_position(p[0], p[1])]
-        diagonal_moves =  [p for p in [(x+1, y+1), (x+1, y-1), (x-1, y+1), (x-1, y-1) ] if self.valid_position(p[0], p[1])]
-        if self.allow_diagonals:
-            return normal_moves + diagonal_moves
-        else:
-            return normal_moves
+    def get_normal_neighbors(self, x, y):
+        return  [p for p in [(x+1, y), (x-1, y), (x, y+1), (x, y-1) ] if self.valid_position(p[0], p[1])]
 
-    def moves(self):
+    def get_diagonal_neighbors(self, x, y):
+        return [p for p in [(x+1, y+1), (x+1, y-1), (x-1, y+1), (x-1, y-1) ] if self.valid_position(p[0], p[1])]
+
+    def get_all_neighbors(self, x, y):
+        if self.allow_diagonals:
+            return self.get_normal_neighbors(x, y) + self.get_diagonal_neighbors(x, y)
+        else:
+            return self.get_normal_neighbors(x, y)
+
+    def get_moves(self):
         possible_moves = []
         for y, row in enumerate(self.board):
             for x, field in enumerate(row):
                 if not field == self.current_player:
                     continue
-                possible_moves.extend([n for n in self.get_neighbor_fields(x, y) if self.board[n[1]][n[0]] == Board.EMPTY])
+                possible_moves.extend([((x,y), n) for n in self.get_all_neighbors(x, y) if self.board[n[1]][n[0]] == Board.EMPTY])
         return possible_moves
+
+    def _value(self, x, y):
+        return self.board[y][x]
+
+    def set_player_at(self, x, y, player):
+        if not self._value(x, y) == self.current_player and self._value(x, y) == Board.EMPTY:
+            self.board[y][x] = player
+
+    def move(self, move):
+        (from_x, from_y), (to_x, to_y) = move
+        if not self.valid_position(from_x, from_y) or not self.valid_position(to_x, to_y):
+            return "Invalid move"
+
+        if not self._value(from_x, from_y) == self.current_player:
+            return "Invalid move"
+
+        if not self._value(to_x, to_y) == Board.EMPTY:
+            return "Invalid move"
+
+        to_neighbors = self.get_all_neighbors(to_x, to_y)
+        if not (from_x, from_y) in to_neighbors:
+            return "Invalid move"
+
+        changes = []
+
+        to_neighbors = self.get_normal_neighbors(to_x, to_y)
+        for n_x, n_y in to_neighbors:
+            if self._value(n_x, n_y) and not self._value(n_x, n_y) == self.current_player:
+                self.set_player_at(n_x, n_y, self.current_player)
+                changes.append((n_x, n_y))
+
+        to_neighbors = self.get_diagonal_neighbors(to_x, to_y)
+        for diag_x, diag_y in to_neighbors:
+            if self._value(diag_x, diag_y) and not self._value(diag_x, diag_y) == self.current_player:
+                diag_n = self.get_normal_neighbors(diag_x, diag_y)
+                if len(set(changes).intersect(diag_n)) == 2:
+                    self.set_player_at(diag_x, diag_y, self.current_player)
+                    changes.append((diag_x, diag_y))
+
+        self.history.append((move, changes))
+
+        return self
 
                 
 def test_empty_board_moves():
     board = Board()
-    moves  = board.moves()
+    moves  = board.get_moves()
     assert len(moves) == 8
 
     board = Board(allow_diagonals=True)
-    moves  = board.moves()
+    moves  = board.get_moves()
     assert len(moves) == 18
 
 def test_board_sizes():
@@ -74,18 +120,26 @@ def test_board_valid_position():
 
 def test_board_neighbor_fields():
     board = Board()
-    assert board.get_neighbor_fields(0, 0) == [(1,0), (0,1)]
-    assert board.get_neighbor_fields(1, 0) == [(2,0), (0,0), (1, 1)]
-    assert board.get_neighbor_fields(2, 3) == [(3, 3), (1, 3), (2, 4), (2, 2)]
+    assert board.get_normal_neighbors(0, 0) == [(1,0), (0,1)]
+    assert board.get_normal_neighbors(1, 0) == [(2,0), (0,0), (1, 1)]
+    assert board.get_normal_neighbors(2, 3) == [(3, 3), (1, 3), (2, 4), (2, 2)]
 
     board = Board(allow_diagonals=True)
-    assert board.get_neighbor_fields(0, 0) == [(1,0), (0,1), (1,1)]
-    assert board.get_neighbor_fields(2, 3) == [(3, 3), (1, 3), (2, 4), (2, 2), (3, 4), (3, 2), (1, 4), (1, 2)]
+    assert board.get_all_neighbors(0, 0) == [(1,0), (0,1), (1,1)]
+    assert board.get_all_neighbors(2, 3) == [(3, 3), (1, 3), (2, 4), (2, 2), (3, 4), (3, 2), (1, 4), (1, 2)]
+
+
+def test_board_move():
+    board = Board()
+    assert board.move(((-1,0), (0,0))) == "Invalid move"
+    assert board.move(((4,0), (3,0))) == "Invalid move"
+    assert board.move(((1,0), (3,0))) == "Invalid move"
 
 
 test_empty_board_moves()
 test_board_sizes()
 test_board_valid_position()
 test_board_neighbor_fields()
+test_board_move()
             
                     
